8f6c204029695a11abd198e1e583af041be9bc3c
Mustafa Melih Çetin <melih.cetin@outlook.com.tr>
2024-05-12 18:35:55 +0300

    .

--- DSMyCoursesLinkedList/DSMyCoursesLinkedList.cpp
+++ DSMyCoursesLinkedList/DSMyCoursesLinkedList.cpp
@@ -1,202 +1,188 @@
 #include <iostream>
 using namespace std;
 
+// Node class to represent individual nodes in the linked list
 class Node
 {
 public:
-	int value; // Value stored in the node
-	Node* next; // Pointer to the next node
+    int value; // Value stored in the node
+    Node* next; // Pointer to the next node
 
-	// Constructor to initialize the node with a value
-	Node(int value) {
-		this->value = value;
-		next = NULL;
-	}
+    // Constructor to initialize the node with a value
+    Node(int value) : value(value), next(nullptr) {}
 };
 
+// LinkedList class to manage the linked list operations
 class LinkedList {
 private:
-	Node* head; // Pointer to the first node in the list
-	Node* tail; // Pointer to the last node in the list
-	int length; // Length of the list
+    Node* head; // Pointer to the first node in the list
+    Node* tail; // Pointer to the last node in the list
+    int length; // Length of the list
 
 public:
-	// Constructor to create a new linked list with an initial value
-	LinkedList(int value) {
-		Node* node = new Node(value);
-		head = node;
-		tail = node;
-		length = 1;
-	}
+    // Constructor to create a new linked list with an initial value
+    LinkedList(int value) : head(nullptr), tail(nullptr), length(0) {
+        appendList(value);
+    }
 
-	// Destructor to delete all nodes in the list
-	~LinkedList() {
-		cout << "Destructor is called" << endl;
-		Node* tmp = head;
-		while (head != NULL) {
-			head = head->next;
-			delete tmp;
-			tmp = head;
-		}
-	}
+    // Destructor to delete all nodes in the list
+    ~LinkedList() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
 
-	// Method to print the value of the first node (head) in the list
-	void printHead() {
-		if (head != NULL) {
-			cout << "Head: " << head->value << endl;
-		}
-	}
+    // Method to append a new node with the given value to the end of the list
+    void appendList(int value) {
+        Node* node = new Node(value);
+        if (isEmpty()) {
+            head = node;
+        }
+        else {
+            tail->next = node;
+        }
+        tail = node;
+        length++;
+    }
 
-	// Method to print the value of the last node (tail) in the list
-	void printTail() {
-		if (tail != NULL) {
-			cout << "Tail: " << tail->value << endl;
-		}
-	}
+    // Method to print all values in the list
+    void printList() {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->next == tail->next) {
+                cout << current->value << endl;
+                return;
+            }  
+            else {
+                cout << current->value << "-->";
+                current = current->next;
+            }            
+        }
+        cout << endl;
+    }
 
-	// Method to get the length of the list
-	void getLength() {
-		cout << "Length of the list: " << this->length << endl;
-	}
+    // Method to check if the list is empty
+    bool isEmpty() const {
+        return head == nullptr;
+    }
 
-	// Method to append a new node with the given value to the end of the list
-	void appendList(int value) {
-		Node* node = new Node(value);
-		if (length == 0) {
-			head = node;
-			tail = node;
-		}
-		else {
-			tail->next = node;
-			tail = node;
-		}
-		length++;
-	}
+    // Method to delete the last node in the list
+    void deleteLastNode() {
+        if (!isEmpty()) {
+            Node* current = head;
+            Node* previous = nullptr;
+            while (current->next != nullptr) {
+                previous = current;
+                current = current->next;
+            }
+            if (previous != nullptr) {
+                previous->next = nullptr;
+                tail = previous;
+            }
+            else {
+                head = nullptr;
+                tail = nullptr;
+            }
+            delete current;
+            length--;
+            cout << "Last node deleted." << endl;
+        }
+    }
 
-	// Method to print all values in the list
-	void printList() {
-		Node* tmp = head;
-		while (tmp != NULL) {
-			cout << tmp->value << "--";
-			tmp = tmp->next;
-		}
-		cout << endl;
-	}
-	void deleteLastNode() {
-		if (length == 0)
-			return;
-		Node* tmp1 = head;
-		Node* tmp2 = head;
-		while (tmp1->next) {
-			tmp2 = tmp1;
-			tmp1 = tmp1->next;
-		}
-		tail = tmp2;
-		tail->next = NULL;
-		length--;
+    // Method to add a new node at the beginning of the list
+    void addFirstNode(int value) {
+        Node* node = new Node(value);
+        if (isEmpty()) {
+            tail = node;
+        }
+        node->next = head;
+        head = node;
+        length++;
+    }
 
-		cout << "Last node is deleted." << endl;
-		if (length == 0) {
-			head = NULL;
-			tail = NULL;
-		}
-		delete tmp1;
-	}
-	void AddFirstNode(int value) {
-		Node* node = new Node(value);
-		if (length == 0) {
-			head = node;
-			tail = node;
-		}
-		else {
-			node->next = head;
-			head = node;
-		}
-		length++;
-	}
-	void DeleteFirst() {
-		if (length == 0) {
-			return;
-		}
-		Node* temp = head;
-		if (length == 1) {
-			head = NULL;
-			tail = NULL;
-		}
-		else {
-			head = head->next;
-		}
-		delete temp;
-		length--;
-	}
+    // Method to delete the first node in the list
+    void deleteFirst() {
+        if (!isEmpty()) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            length--;
+            cout << "First node deleted." << endl;
+        }
+    }
 
-	void GetNode(int index) {
-		//Boundary condition check
-		if (index < 0 || index >= length) {
-			return;
-		}
-		Node* tmp1 = head;
-		Node* tmp2 = head;
-		int count = 0;
-		while (tmp1->next) {
-			if (count == index)
-			{
-				cout << index << ". node is " << tmp1->value;
-			}
-			else {
+    // Method to get the value of a node at a specific index
+    void getNode(int index) {
+        if (index >= 0 && index < length) {
+            Node* current = head;
+            for (int i = 0; i < index; i++) {
+                current = current->next;
+            }
+            cout << "Value at index " << index << ": " << current->value << endl;
+        }
+        else {
+            cout << "Invalid index." << endl;
+        }
+    }
 
-				tmp2 = tmp1;
-				tmp1 = tmp1->next;
-			}
-
-			count++;
-		}
-	}
+    // Method to change the value of a node at a specific index
+    void changeNodeValue(int index, int newValue) {
+        if (index >= 0 && index < length) {
+            Node* current = head;
+            for (int i = 0; i < index; i++) {
+                current = current->next;
+            }
+            current->value = newValue;
+            cout << "Value at index " << index << " changed to " << newValue << endl;
+        }
+        else {
+            cout << "Invalid index." << endl;
+        }
+    }
 };
 
 int main()
 {
-	// Create a new linked list with an initial value of 12
-	LinkedList* LinkedList1 = new LinkedList(12);
+    // Create a new linked list with an initial value of 12
+    LinkedList myList(12);
 
-	// Append additional values to the list
-	LinkedList1->appendList(5);
-	LinkedList1->appendList(24);
-	LinkedList1->appendList(8);
-	LinkedList1->appendList(7);
+    // Append additional values to the list
+    myList.appendList(5);
+    myList.appendList(24);
+    myList.appendList(8);
+    myList.appendList(7);
 
+    // Print the entire list
+    myList.printList();
 
-	// Print the entire list
-	LinkedList1->printList();
+    // Delete the last node from the list
+    myList.deleteLastNode();
 
-	// Get and print the length of the list
-	LinkedList1->getLength();
+    // Print the list after deletion
+    myList.printList();
 
-	// Print the value of the first node (head)
-	LinkedList1->printHead();
+    // Add a new node to the beginning of the list
+    myList.addFirstNode(10);
 
-	// Print the value of the last node (tail)
-	LinkedList1->printTail(); cout << endl;
+    // Print the list after adding a node at the beginning
+    myList.printList();
 
-	//Delete the last node
-	LinkedList1->deleteLastNode(); cout << endl;
+    // Delete the first node from the list
+    myList.deleteFirst();
 
-	// Print the entire list
-	LinkedList1->printList(), cout << endl;;
+    // Print the list after deleting the first node
+    myList.printList();
 
-	//Add a new node to the head of the List
-	LinkedList1->AddFirstNode(10);
-	LinkedList1->printList(), cout << endl;;
+    // Get the value of a node at index 0
+    myList.getNode(0);
 
-	// Delete first node from the list
-	LinkedList1->DeleteFirst();
-	LinkedList1->printList(), cout << endl;;
+    // Change the value of a node at index 2
+    myList.changeNodeValue(2, 40);
 
-	LinkedList1->GetNode(0), cout << endl;
+    // Print the final list after all operations
+    myList.printList();
 
-	// Delete the linked list to free memory
-	delete LinkedList1;
-
-
-	return 0;
+    return 0;
 }
